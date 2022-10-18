4ba68d70f0ec34573caa3bf9c0dcb56d398ec34d
Yiannis Charalambous <yiannis128@hotmail.com>
2022-10-18 14:29:52 +0100

    nvim cmp updated config to new api config

--- plugin/cmp.lua
+++ plugin/cmp.lua
@@ -5,7 +5,7 @@ cmp.setup({
   snippet = {
     -- REQUIRED - you must specify a snippet engine
     expand = function(args)
-      vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+      -- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
       -- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
       -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
       -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
@@ -32,7 +32,7 @@ cmp.setup({
   }),
   sources = cmp.config.sources({
     { name = 'nvim_lsp' },
-    { name = 'vsnip' }, -- For vsnip users.
+    -- { name = 'vsnip' }, -- For vsnip users.
     -- { name = 'luasnip' }, -- For luasnip users.
     -- { name = 'ultisnips' }, -- For ultisnips users.
     -- { name = 'snippy' }, -- For snippy users.
@@ -69,13 +69,50 @@ cmp.setup.cmdline(':', {
 })
 
 -- Set up lspconfig.
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
 -- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
+
 require('lspconfig')['ltex'].setup {
   capabilities = capabilities,
-  language = 'en-GB',
-  dictionary = { ['en-US'] = { 'Datapump', 'datapump', 'Neovim', 'ltex-ls' } },
+  settings = {
+    ltex = {
+      language = 'en-GB',
+      dictionary = { ['en-US'] = { 'Datapump', 'datapump', 'Neovim', 'ltex-ls' } },
+    }
+  }
 }
+
 require('lspconfig')['pyright'].setup {
   capabilities = capabilities,
 }
+
+require('lspconfig')['sumneko_lua'].setup {
+  settings = {
+    Lua = {
+      runtime = {
+        -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+        version = 'LuaJIT',
+      },
+      diagnostics = {
+        -- Get the language server to recognize the `vim` global
+        globals = {'vim'},
+      },
+      workspace = {
+        -- Make the server aware of Neovim runtime files
+        library = vim.api.nvim_get_runtime_file("", true),
+      },
+      -- Do not send telemetry data containing a randomized but unique identifier
+      telemetry = {
+        enable = false,
+      },
+    },
+  },
+}
+
+
+
+
+
+
+
